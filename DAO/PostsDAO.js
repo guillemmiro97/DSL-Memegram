d86bdc8595a590e1d28d10595301f7c151a27995
guillemmiro <guillem.miro@students.salle.url.edu>
2022-04-05 13:58:11 +0200

    Implemented all the functions for the posts module.

--- DAO/PostsDAO.js
+++ DAO/PostsDAO.js
@@ -4,6 +4,38 @@ class PostsDAO extends GenericDAO {
     constructor() {
         super("post")
     }
+
+    async getPostsByUser(id){
+        this._id = id;
+        // SELECT * FROM ?? WHERE id = 'params.id'
+        const [results] = await global.connection.promise().query("SELECT * FROM ?? WHERE idUser = ?", [this.tabla, this._id])
+        return results;
+    }
+
+    async howManyPosts(id){
+        this._id = id;
+        // SELECT COUNT(*) FROM ?? WHERE id = 'params.id'
+        const [results] = await global.connection.promise().query("SELECT COUNT(*) FROM ?? WHERE idUser = ?", [this.tabla, this._id])
+        return results;
+    }
+
+    async insertPost(userid, uidimagen){
+        this._userid = userid
+        this._uidimagen = uidimagen
+        // insert into post (iduser, uidimagen) values (?, ?);
+        const [results] = await global.connection.promise()
+            .query("INSERT INTO ?? (iduser, uidimagen) VALUES (?, ?)", [this.tabla, this._userid, this._uidimagen])
+        return results;
+    }
+
+    async updatePost(id, imageId){
+        this._id = id
+        this._imageId = imageId
+        // UPDATE POST SET UIDimagen = imageId WHERE id = id;
+        const [results] = await global.connection.promise()
+            .query("UPDATE ?? SET UIDimagen = ? WHERE  id = ?", [this.tabla, this._imageId,  this._id])
+        return results
+    }
 }
 
 module.exports = PostsDAO
--- routes/posts_route.js
+++ routes/posts_route.js
@@ -2,27 +2,38 @@ const express = require("express")
 const router = express.Router()
 
 const PostsDAO = require("../DAO/PostsDAO")
+const req = require("express/lib/request");
 const pdao = new PostsDAO()
 
 router.get("/", async (req, res, next) => {
     res.json(await pdao.getAll())
 })
 
-router.get("/:id", (req, res, next) => {
-    next("not implemented yet")
+router.get("/:id", async (req, res, next) => {
+    res.json(await pdao.get(req.params.id))
 })
 
-router.post("/", (req, res, next) => {
-    next("not implemented yet")
+router.get("/getuserposts/:id", async (req, res, next) => {
+    res.json(await pdao.getPostsByUser(req.params.id))
 })
 
-
-router.put("/:id", (req, res, next) => {
-    next("not implemented yet")
+router.get("/howmanyposts/:id", async (req, res, next) => {
+    res.json(await pdao.howManyPosts(req.params.id))
 })
 
-router.delete("/:id", (req, res, next) => {
-    next("not implemented yet")
+router.post("/", async (req, res, next) => {
+    console.log(req.body)
+    res.json(await pdao.insertPost(req.body.iduser, req.body.uidimagen))
+})
+
+router.put("/:id", async (req, res, next) => {
+    console.log(req.body, req.params.id)
+    res.json(await pdao.updatePost(req.params.id, req.body.uidimagen))
+})
+
+router.delete("/:id", async (req, res, next) => {
+    console.log(req.body, req.params.id)
+    res.json(await pdao.delete(req.params.id))
 })
 
 
